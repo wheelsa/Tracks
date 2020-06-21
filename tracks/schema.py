e5c94080bb452e0da6d9d4fe578b825fc624c136
Seth Wheeler <wheeler.seth@gmail.com>
2020-06-21 07:14:04 -0600

    added update, delete, and add track w user

--- tracks/schema.py
+++ tracks/schema.py
@@ -22,9 +22,61 @@ class CreateTrack(graphene.Mutation):
     url = graphene.String()
 
   def mutate(self, info, title, description, url):
-    track = Track(title=title, description=description, url=url)
+    user = info.context.user
+    
+    if user.is_anonymous: 
+      raise Exception('Please log in to add a track.')
+
+
+    track = Track(title=title, description=description, url=url, posted_by=user)
     track.save()
     return CreateTrack(track=track)
 
+
+class UpdateTrack(graphene.Mutation):
+  track = graphene.Field(TrackType)
+
+  class Arguments:
+
+    track_id = graphene.Int(required=True)
+    title = graphene.String()
+    description = graphene.String()
+    url = graphene.String()
+
+  def mutate(self, info, track_id, title, url, description):
+    user = info.context.user
+    track = Track.objects.get(id=track_id)
+
+    if track.posted_by != user:
+      raise Exception('Not permitted to updated this track.')
+
+    track.title = title
+    track.description = description
+    track.url = url
+
+    track.save()
+
+    return UpdateTrack(track=track)
+
+class DeleteTrack(graphene.Mutation):
+  track_id = graphene.Int()
+
+  class Arguments:
+    track_id = graphene.Int(required=True)
+
+  def mutate(self,info, track_id):
+    user = info.context.user
+    track = Track.objects.get(id = track_id)
+
+    if track.posted_by != user:
+
+      raise Exception('Not permitted to delete this track.')
+
+    track.delete()
+
+    return DeleteTrack(track_id=track_id)
+
 class Mutation(graphene.ObjectType):
   create_track = CreateTrack.Field()
+  update_track = UpdateTrack.Field()
+  delete_track = DeleteTrack.Field()
